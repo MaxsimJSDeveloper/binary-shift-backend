ec4b357d1a68438177e8971d371f261e1f413a15
Holovko Maksym <golovkomaksim852@gmail.com>
2024-08-30 19:30:14 +0300

    fix

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -2,10 +2,14 @@ import { model, Schema } from 'mongoose';
 
 const usersSchema = new Schema(
   {
-    name: { type: String,},
+    name: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, minlength: 8, maxlength: 64, required: true },
-    gender: { type: String, enum: ['male', 'female', 'other'], required: true },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'other'],
+      default: 'female',
+    },
     photo: { type: String, default: '' },
     dailyNorma: { type: Number, default: 2000 },
   },
