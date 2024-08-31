fa827adf85bdf658bee305b91e77af3be7abe05b
Alexaanaax <149320517+Alexaanaax@users.noreply.github.com>
2024-08-31 19:51:52 +0200

    get-water-rate

--- src/services/waterRate.js
+++ src/services/waterRate.js
@@ -1,6 +1,14 @@
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 
+export const getDailyNorma = async (userId) => {
+  const user = await UsersCollection.findById(userId);
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  return user.dailyNorma;
+};
+
 export const updateUser = async (userId, updateData) => {
   const user = await UsersCollection.findByIdAndUpdate(
     userId,
