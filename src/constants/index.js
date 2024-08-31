0173ce9f5fb322bf128c863e3a0eb5af6b3ad6aa
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-08-31 22:21:48 +0200

    Add today

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,3 +20,5 @@ export const CLOUDINARY = {
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
