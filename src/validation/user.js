7b4a87b508cf603874bb33a3b5ea7cd388009b96
Alexaanaax <149320517+Alexaanaax@users.noreply.github.com>
2024-09-05 20:25:30 +0200

    fix

--- src/validation/user.js
+++ src/validation/user.js
@@ -21,8 +21,9 @@ export const userValidationSchema = Joi.object({
 export const updateUserValidationSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
-  password: Joi.string().min(8).max(64),
+  newPassword: Joi.string().min(8).max(64),
   gender: Joi.string().valid('male', 'female').default('female'),
   photo: Joi.string(),
   dailyNorma: Joi.number().positive(),
+  outdatedPassword: Joi.string().min(8).max(64),
 });
